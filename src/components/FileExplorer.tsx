676bd9aea054887af0929402985333ee5db9df6a
Sebastian Christensen Mondrup <semon20@student.sdu.dk>
2023-04-10 12:34:33 +0200

    Hiding folder content by default

--- src/components/FileExplorer.tsx
+++ src/components/FileExplorer.tsx
@@ -1,28 +1,14 @@
 import React, { useState } from 'react';
 import Folder from './Folder';
-import File from './File';
 
 export default function FileExplorer(): JSX.Element {
   const [mainDirectoryHandle, setMainDirectoryHandle] = useState<FileSystemDirectoryHandle>();
-  const [subFolders, setSubfolders] = useState<FileSystemDirectoryHandle[]>();
-  const [files, setFiles] = useState<FileSystemFileHandle[]>();
 
   const selectFolder = (): void => {
-    const newSubFolders: FileSystemDirectoryHandle[] = [];
-    const newFiles: FileSystemFileHandle[] = [];
     window
       .showDirectoryPicker()
-      .then(async (result) => {
+      .then((result) => {
         setMainDirectoryHandle(result);
-        for await (const [_name, value] of result.entries()) {
-          if (value.kind === 'directory') {
-            newSubFolders.push(value);
-          } else if (value.kind === 'file') {
-            newFiles.push(value);
-          }
-        }
-        setSubfolders(newSubFolders);
-        setFiles(newFiles);
       })
       .catch(console.error);
   };
@@ -38,18 +24,13 @@ export default function FileExplorer(): JSX.Element {
       </button>
       <button>Close Folder</button>
       <h2>{mainDirectoryHandle?.name}</h2>
-      <ul className="list-group">
-        {subFolders?.map((directoryHandle) => (
-          <li className="list-group-item" key={directoryHandle.name}>
-            <Folder handle={directoryHandle}></Folder>
+      {mainDirectoryHandle !== undefined && (
+        <ul className="list-group">
+          <li className="list-group-item" key={mainDirectoryHandle?.name}>
+            <Folder handle={mainDirectoryHandle}></Folder>
           </li>
-        ))}
-        {files?.map((fileHandle) => (
-          <li className="list-group-item" key={fileHandle.name}>
-            <File handle={fileHandle}></File>
-          </li>
-        ))}
-      </ul>
+        </ul>
+      )}
     </div>
   );
 }
--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -9,6 +9,7 @@ export default function Folder(props: FolderProps): JSX.Element {
   const [directoryHandle, setDirectoryHandle] = useState<FileSystemDirectoryHandle>();
   const [subFolders, setSubfolders] = useState<FileSystemDirectoryHandle[]>();
   const [files, setFiles] = useState<FileSystemFileHandle[]>();
+  const [expanded, setExpanded] = useState(false);
 
   useEffect(() => {
     setDirectoryHandle(props.handle);
@@ -33,19 +34,28 @@ export default function Folder(props: FolderProps): JSX.Element {
 
   return (
     <div>
-      <h2>{directoryHandle?.name}</h2>
-      <ul className="list-group">
-        {subFolders?.map((directoryHandle) => (
-          <li className="list-group-item" key={directoryHandle.name}>
-            <Folder handle={directoryHandle}></Folder>
-          </li>
-        ))}
-        {files?.map((fileHandle) => (
-          <li className="list-group-item" key={fileHandle.name}>
-            <File handle={fileHandle}></File>
-          </li>
-        ))}
-      </ul>
+      <p>{directoryHandle?.name}</p>
+      <button
+        onClick={() => {
+          setExpanded(!expanded);
+        }}
+      >
+        Expand
+      </button>
+      {expanded && (
+        <ul className="list-group">
+          {subFolders?.map((directoryHandle) => (
+            <li className="list-group-item" key={directoryHandle.name}>
+              <Folder handle={directoryHandle}></Folder>
+            </li>
+          ))}
+          {files?.map((fileHandle) => (
+            <li className="list-group-item" key={fileHandle.name}>
+              <File handle={fileHandle}></File>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
