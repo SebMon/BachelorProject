2efcc22c278ea0a780581b0201e4ce40cdd29071
Magnus Kjær Sørensen <magnus.kjaer310101@gmail.com>
2023-04-25 08:45:56 +0200

    Ensure the file explorer always has the correct height

--- src/components/FileExplorer.tsx
+++ src/components/FileExplorer.tsx
@@ -1,6 +1,8 @@
-import React, { useContext, useMemo, useRef, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import Folder from './Folder';
 import { selectedFileContext } from '../context/SelectedFileContext';
+
+let didInit = false;
 
 export default function FileExplorer(): JSX.Element {
   const [mainDirectoryHandle, setMainDirectoryHandle] = useState<FileSystemDirectoryHandle>();
@@ -20,22 +22,35 @@ export default function FileExplorer(): JSX.Element {
   };
 
   const buttonRowRef = useRef<HTMLDivElement>(null);
+
+  // This useState and useEffect is just a small but ugly wait to get react to recalculate the filesystemheight after having rendered
   const [windowSize, setScreenSize] = useState(0);
+  useEffect(() => {
+    if (!didInit) {
+      didInit = true;
+      setScreenSize(windowSize - 1);
+    }
+  }, []);
 
   window.addEventListener('resize', () => {
     setScreenSize(window.innerWidth);
   });
 
-  const fileSystemMaxHeight = useMemo(() => {
-    if (buttonRowRef !== null) {
-      const buttonRowHeight = buttonRowRef.current?.offsetHeight;
-      if (buttonRowHeight === undefined) {
-        return 0;
-      }
-      return `calc(100% - ${buttonRowHeight + 15}px)`;
+  const calculateFileSystemHeight = (): string => {
+    if (buttonRowRef === null) {
+      return '0';
     }
-    return '0';
-  }, [buttonRowRef, windowSize, mainDirectoryHandle]);
+
+    const buttonRowHeight = buttonRowRef.current?.offsetHeight;
+
+    if (buttonRowHeight === undefined) {
+      return '0';
+    }
+
+    return `calc(100% - ${buttonRowHeight}px - 3em)`;
+  };
+
+  const fileSystemHeight = calculateFileSystemHeight();
 
   return (
     <div className="container h-100">
@@ -63,7 +78,7 @@ export default function FileExplorer(): JSX.Element {
           <i className={'col-1 bi bi-arrow-clockwise'} />
         </button>
       </div>
-      <div className="container pt-3 bg-light rounded overflow-auto" style={{ maxHeight: fileSystemMaxHeight }}>
+      <div className="container pt-3 bg-light rounded overflow-auto" style={{ height: fileSystemHeight }}>
         {mainDirectoryHandle !== undefined ? <Folder handle={mainDirectoryHandle}></Folder> : null}
       </div>
     </div>
