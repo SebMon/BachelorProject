87ff5616e4b075cae35213861587c3b05035def6
Magnus Kjær Sørensen <magnus.kjaer310101@gmail.com>
2023-05-01 09:47:39 +0200

    Stop app from crashing when opened first time

--- src/persistence/settings.tsx
+++ src/persistence/settings.tsx
@@ -26,19 +26,12 @@ export class Settings extends Dexie {
   async getEncryptionEngine(): Promise<EncryptionEngine> {
     const object = await this.settings.get(ENCRYPTION_ENGINE);
 
-    if (object === undefined) {
-      await this.settings.add({ key: ENCRYPTION_ENGINE, value: EncryptionEngine.wasm });
-      return EncryptionEngine.wasm;
-    }
-
-    const value = object.value;
-
-    if (!(value === EncryptionEngine.js || value === EncryptionEngine.wasm)) {
+    if (object === undefined || !(object.value === EncryptionEngine.js || object.value === EncryptionEngine.wasm)) {
       await this.settings.put({ key: ENCRYPTION_ENGINE, value: EncryptionEngine.wasm });
       return EncryptionEngine.wasm;
     }
 
-    return value;
+    return object.value;
   }
 
   async setEncryptionEngine(value: EncryptionEngine): Promise<void> {
