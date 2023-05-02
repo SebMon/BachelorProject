bebe77a02e863562f00750d97401199e5c5f6757
Sebastian Christensen Mondrup <semon20@student.sdu.dk>
2023-05-02 10:10:56 +0200

    Changing to check if public or private key

--- src/encryption/workers/RSA-D-wasm.ts
+++ src/encryption/workers/RSA-D-wasm.ts
@@ -4,6 +4,6 @@ import type { RSARequest } from './types';
 self.onmessage = async (message): Promise<void> => {
   const req = message.data as RSARequest;
   await init();
-  const bytes = rsa_decrypt(req.bytes, req.rsakey.n, req.rsakey.e);
+  const bytes = rsa_decrypt(req.bytes, req.rsakey.n, 'd' in req.rsakey ? req.rsakey.d : req.rsakey.e);
   self.postMessage(bytes);
 };
