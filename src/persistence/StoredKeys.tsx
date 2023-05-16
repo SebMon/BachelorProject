2393cca4c69c8ab3212032f04bcaa2b7d6cccce2
Magnus Kjær Sørensen <magnus.kjaer310101@gmail.com>
2023-05-16 10:28:06 +0200

    Refactor StoredKeys to improve encapsulation

--- src/persistence/StoredKeys.tsx
+++ src/persistence/StoredKeys.tsx
@@ -25,7 +25,7 @@ export function isStoredRSAPrivateKey(storedKey: StoredKey): storedKey is Stored
   return 'n' in storedKey && 'd' in storedKey;
 }
 
-export class StoredKeys extends Dexie {
+class StoredKeysDatabase extends Dexie {
   aesKeys!: Dexie.Table<StoredAESKey, number>;
   rsaPublicKeys!: Dexie.Table<StoredRSAPublicKey, number>;
   rsaPrivateKeys!: Dexie.Table<StoredRSAPrivateKey, number>;
@@ -41,13 +41,21 @@ export class StoredKeys extends Dexie {
       rsaPrivateKeys: '++id, name, n, d, e, p, q, dp, dq, qi'
     });
   }
+}
+
+export class StoredKeys {
+  private readonly db: StoredKeysDatabase;
+
+  constructor() {
+    this.db = new StoredKeysDatabase();
+  }
 
   async getAll(): Promise<StoredKey[]> {
     const emptyArr: StoredKey[] = [];
     const keys = emptyArr.concat(
-      await this.aesKeys.toArray(),
-      await this.rsaPublicKeys.toArray(),
-      await this.rsaPrivateKeys.toArray()
+      await this.db.aesKeys.toArray(),
+      await this.db.rsaPublicKeys.toArray(),
+      await this.db.rsaPrivateKeys.toArray()
     );
     keys.sort((a, b) => a.name.localeCompare(b.name));
     return keys;
@@ -55,15 +63,15 @@ export class StoredKeys extends Dexie {
 
   async store(key: StoredKey): Promise<void> {
     if (isStoredAESKey(key)) {
-      await this.aesKeys.put(key);
+      await this.db.aesKeys.put(key);
       return;
     }
     if (isStoredRSAPublicKey(key)) {
-      await this.rsaPublicKeys.put(key);
+      await this.db.rsaPublicKeys.put(key);
       return;
     }
     if (isStoredRSAPrivateKey(key)) {
-      await this.rsaPrivateKeys.put(key);
+      await this.db.rsaPrivateKeys.put(key);
       return;
     }
     throw Error('The key was not of a type that could be stored in the database');
@@ -74,15 +82,15 @@ export class StoredKeys extends Dexie {
       throw Error("Can't remove a key without an id");
     }
     if (isStoredAESKey(key)) {
-      await this.aesKeys.delete(key.id);
+      await this.db.aesKeys.delete(key.id);
       return;
     }
     if (isStoredRSAPublicKey(key)) {
-      await this.rsaPublicKeys.delete(key.id);
+      await this.db.rsaPublicKeys.delete(key.id);
       return;
     }
     if (isStoredRSAPrivateKey(key)) {
-      await this.rsaPrivateKeys.delete(key.id);
+      await this.db.rsaPrivateKeys.delete(key.id);
       return;
     }
     throw Error('The key was not of a type that could be in the database');
